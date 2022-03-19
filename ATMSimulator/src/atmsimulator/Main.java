7cae20e843c7efbbb7aa18608b9d9cd076337ae5
eduard_blumental <eduard.blumental@gmail.com>
2022-03-19 10:31:30 +0200

    Why you need to do git pull before git push.

--- ATMSimulator/src/atmsimulator/Main.java
+++ ATMSimulator/src/atmsimulator/Main.java
@@ -3,7 +3,7 @@ package atmsimulator;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Some day it will be an ATM simulator.");
+        System.out.println("Some day it will be an ATM simulator."); 
     }
 
 }
