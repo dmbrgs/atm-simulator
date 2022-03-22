0e8b29b7ce53fe9e51d25f05124c3dc214975d49
eduard_blumental <eduard.blumental@gmail.com>
2022-03-22 22:02:59 +0200

    ATM class PIN checking functionality implemented.

--- ATMSimulator/src/atmsimulator/ATM.java
+++ ATMSimulator/src/atmsimulator/ATM.java
@@ -1,8 +1,11 @@
 package atmsimulator;
 
+import java.util.Scanner;
+
 public class ATM {
 
     private double cashInATM;
+    private Card card;
 
     public ATM(double cashInATM) {
         this.cashInATM = cashInATM;
@@ -21,8 +24,31 @@ public class ATM {
         return "ATM{" + "cashInATM=" + cashInATM + '}';
     }
     
-    public void acceptCard(Card card){
-        
+    public void acceptCard(Card card) {
+        this.card = card;
     }
-
+    
+    private boolean isValidPIN(String userPIN) {
+        return this.card.getPINCode().equals(userPIN);
+    }
+    
+    public boolean checkPIN() {
+        Scanner scanner = new Scanner(System.in);
+        
+        for (int i=0; i < 3; i++) {
+            System.out.println("Enter PIN: ");
+            String userPIN = scanner.nextLine();
+            
+            if (this.isValidPIN(userPIN)) {
+                System.out.println("Success");
+                return true;
+            }
+            
+            if (i < 2)
+                System.out.println("Wrong PIN, please try again.\n");
+        }
+        
+        System.out.println("Wrong PIN. Bank alert sent. Run.");
+        return false;
+    }    
 }
--- ATMSimulator/src/atmsimulator/Main.java
+++ ATMSimulator/src/atmsimulator/Main.java
@@ -7,11 +7,14 @@ public class Main {
         System.out.println("Some day it will be an ATM simulator.");
 
         Card newCard = new Card("Roberts Dambergs", "1234567890123456", "03/25", "559", "0045" , 500.0);
-        System.out.println(newCard.toString());
-        System.out.println(newCard.getCardPrivateDetails());
-        System.out.println(newCard.getCardPAN());
-        newCard.setAccountBalance(1000000.0);
-        System.out.println(newCard.getCardPrivateDetails());
+        ATM myATM = new ATM(10000);
+        
+        myATM.acceptCard(newCard);
+        if (myATM.checkPIN()) {
+            System.out.println("Programma trurpinajas");
+        } else {
+            System.out.println("Programma beidzas");
+        }
     }
 
 }
