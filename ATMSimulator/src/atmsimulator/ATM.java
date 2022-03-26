38733ce63aa92ea0a282e2f555defdc127545a06
eduard_blumental <eduard.blumental@gmail.com>
2022-03-26 09:36:19 +0200

    Silly me v2.

--- ATMSimulator/src/atmsimulator/ATM.java
+++ ATMSimulator/src/atmsimulator/ATM.java
@@ -1,7 +1,5 @@
 package atmsimulator;
 
-import java.util.Scanner;
-
 public class ATM {
 
     private double cashInATM;
@@ -23,34 +21,4 @@ public class ATM {
     public String toString() {
         return "ATM{" + "cashInATM=" + cashInATM + '}';
     }
-    
-    public void acceptCard(Card card) {
-        this.card = card;
-    }
-    
-    private boolean isValidPIN(String userPIN) {
-        return this.card.getPINCode().equals(userPIN);
-    }
-    
-    private String askUserPIN() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter PIN: ");
-        String userPIN = scanner.nextLine();
-        return userPIN;
-    }
-    
-    public boolean checkPIN() {
-        for (int i=0; i < 3; i++) {
-            String userPIN = this.askUserPIN();
-            
-            if (this.isValidPIN(userPIN)) {
-                System.out.println("Success");
-                return true;
-            }
-            
-            if (i < 2) System.out.println("Wrong PIN, please try again.\n");
-        }
-        
-        return false;
-    }    
 }
--- ATMSimulator/src/atmsimulator/Main.java
+++ ATMSimulator/src/atmsimulator/Main.java
@@ -9,10 +9,7 @@ public class Main {
         Card newCard = new Card("Roberts Dambergs", "1234567890123456", "03/25", "559", "0045" , 500.0);
         ATM myATM = new ATM(10000);
         
-        myATM.acceptCard(newCard);
-        boolean correctPIN = myATM.checkPIN();
-        
-        if (correctPIN) {
+        if (true) {
             System.out.println("Programma trurpinajas");
         } else {
             System.out.println("Programma beidzas");
